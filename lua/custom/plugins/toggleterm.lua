afa9584cd4fbebe24c8599284770fdb96359becb
pbharrell <backstroker2001@gmail.com>
2024-09-11 14:17:35 -0400

    Adjusted toggleterm behavior for multiple terminals

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,12 +2,20 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
-    require('toggleterm').setup()
+    require('toggleterm').setup {
+      open_mapping = [[<c-\>]],
+    }
     vim.keymap.set('n', 'th', ':ToggleTerm size=20 direction=horizontal<CR>', { desc = 'Toggle [H]orizontal terminal' })
     vim.keymap.set('n', 'tv', ':ToggleTerm size=60 direction=vertical<CR>', { desc = 'Toggle [V]ertical terminal' })
     vim.keymap.set('n', 'tf', ':ToggleTerm direction=float<CR>', { desc = 'Toggle [V]ertical terminal' })
-    -- pass through <Esc> to lazygit when that is open
 
+    -- make inter-terminal navigation easier without leaving terminal mode
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
+
+    -- pass through <Esc> to toggleterm when that is open
     vim.keymap.set('t', '<Esc>', function()
       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
     end, { noremap = true, silent = true })
