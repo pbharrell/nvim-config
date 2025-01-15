b605c6a6a85f37e790b71c4128ee5df39a6fc1c3
Preston Harrell <preston.harrell@garmin.com>
2025-01-15 12:20:14 -0500

    no description bugged me

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -1,7 +1,7 @@
 return {
   'mbbill/undotree',
   config = function()
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Open [U]ndotree' })
     vim.g.undotree_WindowLayout = 1
     vim.undotree_ShortIndicators = true
     vim.g.undotree_SetFocusWhenToggle = true
