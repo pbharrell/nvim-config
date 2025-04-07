fda8e3d6132ef71c2609d16a296f66257c33b37c
Preston Harrell <preston.harrell@garmin.com>
2025-04-07 09:52:58 -0400

    Add "remove auto session' binding

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -7,6 +7,7 @@ return {
       suppressed_dirs = { '~/', '~/Downloads', '/' },
     }
     vim.keymap.set('n', '<leader>ss', '<cmd>SessionSearch<CR>', { desc = '[S]earch [S]essions Telescope' })
+    vim.keymap.set('n', '<leader>sr', '<cmd>SessionDelete<CR>', { desc = '[S]ession [R]emove' })
     vim.o.sessionoptions = 'blank,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
   end,
 }
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -85,7 +85,6 @@ return {
       vim.keymap.set('n', '<leader>sc', function()
         builtin.grep_string { search = vim.fn.getreg '+' }
       end, { desc = '[S]earch current [C]lipboard' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.resume, { desc = '[ ] Resume previous session' })
 
