793916982a555b44ebcc8020681b69bd72a8523a
pbharrell <backstroker2001@gmail.com>
2025-09-30 15:20:14 -0400

    Introduce new auto-session keybinds

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -6,8 +6,8 @@ return {
     require('auto-session').setup {
       suppressed_dirs = { '~/', '~/Downloads', '/' },
     }
-    vim.keymap.set('n', '<leader>ss', '<cmd>SessionSearch<CR>', { desc = '[S]earch [S]essions Telescope' })
-    vim.keymap.set('n', '<leader>sr', '<cmd>SessionDelete<CR>', { desc = '[S]ession [R]emove' })
+    vim.keymap.set('n', '<leader>ss', '<cmd>AutoSession save<CR>', { desc = '[S]ession [S]ave' })
+    vim.keymap.set('n', '<leader>sr', '<cmd>AutoSession delete<CR>', { desc = '[S]ession [R]emove' })
     vim.o.sessionoptions = 'blank,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
   end,
 }
