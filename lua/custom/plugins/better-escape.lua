ab8875a1c4374aac1ae6b4e15b1f24596950c597
pbharrell <backstroker2001@gmail.com>
2025-11-11 14:05:52 -0500

    Some plugin clean-up

---
+++ lua/custom/plugins/better-escape.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    'max397574/better-escape.nvim',
+    -- Using default mappings for this - https://github.com/max397574/better-escape.nvim
+    config = true,
+  },
+}
--- lua/custom/plugins/scrollview.lua
+++ lua/custom/plugins/scrollview.lua
@@ -2,7 +2,7 @@ return {
   'dstein64/nvim-scrollview',
   config = function()
     require('scrollview').setup {
-      signs_on_startup = { 'diagnostics', 'marks', 'search' },
+      signs_on_startup = { 'diagnostics', 'search' },
     }
   end,
 }
