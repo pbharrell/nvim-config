df8b3b6573545814504d162c752b938a8ee9f58b
Preston Harrell <preston.harrell@garmin.com>
2026-04-10 13:17:10 -0500

    Bring back Sleuth (I missed you 🥲)

--- lua/custom/plugins/better-escape.lua
+++ lua/custom/plugins/better-escape.lua
@@ -1,7 +1,5 @@
 return {
-  {
-    'max397574/better-escape.nvim',
-    -- Using default mappings for this - https://github.com/max397574/better-escape.nvim
-    config = true,
-  },
+  'max397574/better-escape.nvim',
+  -- Using default mappings for this - https://github.com/max397574/better-escape.nvim
+  config = true,
 }
