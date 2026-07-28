43692ea21f5a9ae47cdc69b8f6060da2422309c0
pbharrell <backstroker2001@gmail.com>
2026-07-28 10:29:03 -0400

    Ensure more languages installed

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'c', 'cpp' },
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python' },
       highlight = { enable = true },
     }
   end,
