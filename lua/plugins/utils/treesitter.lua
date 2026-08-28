e4cb3fa7e89df6fb421ea366f3dd52943d4b66d9
Preston Harrell <preston.harrell@garmin.com>
2026-08-28 08:01:15 -0500

    Restore Tree-sitter parser installation

---
+++ lua/plugins/utils/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    local treesitter = require 'nvim-treesitter'
+
+    treesitter.setup()
+    treesitter.install { 'c', 'cpp', 'go', 'lua', 'python' }
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'c', 'cpp', 'go', 'lua', 'python' },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
+}
